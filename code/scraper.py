557e1d2e62d7c53c4fedd3f1293db36e733073b0
LikeToAccess <37591675+LikeToAccess@users.noreply.github.com>
2021-07-15 02:03:30 -0500

    Removed comment

--- code/scraper.py
+++ code/scraper.py
@@ -23,7 +23,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 # from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 # from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import *#TimeoutException, ElementClickInterceptedException, NoSuchElementException, NoSuchWindowException
+from selenium.common.exceptions import *
 
 
 class Scraper:

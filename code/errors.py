684d821a31ec6503df5425aab88afdcfc6d27feb
LTA <37591675+LikeToAccess@users.noreply.github.com>
2021-09-08 11:39:14 -0500

    Create errors.py

---
+++ code/errors.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# filename          : errors.py
+# description       : Contains custom Exceptions
+# author            : LikeToAccess
+# email             : liketoaccess@protonmail.com
+# date              : 08-24-2021
+# version           : v1.0
+# usage             : python main.py
+# notes             :
+# license           : MIT
+# py version        : 3.8.2 (must run on 3.6 or higher)
+#==============================================================================
+
+
+class TV_Show_Error(Exception):
+	pass
